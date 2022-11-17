a335734c143d60f41a8b0c90ebaea945a6586952
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-11-17 11:27:28 +0000

    #3 Remove redundant error handler

--- ros2viz/__init__.py
+++ ros2viz/__init__.py
@@ -1,9 +1,3 @@
-try:
-    from importlib.metadata import version
-except ImportError:
-    from importlib_metadata import version
+from importlib.metadata import version
 
-try:
-    __version__ = version("ros2viz")
-except:  # noqa: E722
-    __version__ = "0.0.0"
+__version__ = version("ros2viz")
