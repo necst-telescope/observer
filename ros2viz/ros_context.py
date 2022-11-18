6745277f3c546b2428edfdcfef92f5aede077377
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-11-18 07:37:22 +0000

    #3 Change ROS node creation policy

--- ros2viz/ros_context.py
+++ ros2viz/ros_context.py
@@ -1,15 +1,42 @@
+from threading import Event, Thread
+
 import rclpy
+from rclpy.executors import SingleThreadedExecutor
 from rclpy.node import Node
 
 
-class ros2node:
-    def __enter__(self) -> Node:
+class ros2node(Node):
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self) -> None:
+        super().__init__("ros2viz")
+
+
+class ros2env:
+    def __enter__(self) -> None:
         rclpy.init()
-        self.node = rclpy.create_node("ros2viz_node")
-        return self.node
+        self.thread = Thread(target=self.run, daemon=True)
+        self.event = Event()
+        self.node = ros2node()
+        self.executor = SingleThreadedExecutor()
+        self.executor.add_node(self.node)
+        self.thread.start()
+
+    def run(self) -> None:
+        while not self.event.is_set():
+            self.executor.spin_once(0.05)
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
-        self.node.destroy_node()
+        self.event.set()
+        self.thread.join()
+        [node.destroy_node() for node in self.executor.get_nodes()]
+        [self.executor.remove_node(node) for node in self.executor.get_nodes()]
+        self.executor.shutdown()
         rclpy.shutdown()
         if exc_type is not None:
             raise
