624e8485f590f0a4fb439cfebe02c899c70d1c89
Tatsumi ISHIKAWA <112744259+TatsumiISHIKAWA@users.noreply.github.com>
2025-03-07 19:29:11 +0900

    #92 add sis iv indexjs

--- observer/static/index.js
+++ observer/static/index.js
@@ -56,7 +56,26 @@ function sub2() {
         Graph("#chart", socket).push(msg.topic_name, msg.data, role)
     })
 }
+function sub3() {
+    const socket = io("/qlook")
+    const role = "sis_iv"
+    $("#sis_iv").click(
+        () => {
+            // TODO: Update chart in 2D-plot mode from 1D-plot mode.
+            Graph("#chart", socket).clear()
+            socket.emit("ros2-topic-list-request", { "role": role })
+        }
+    )
+
+    socket.on("ros2-topic-list", msg => quickLook.updateTopicList(socket, msg))
+    // TODO: Do not display buttons of fields.
+    socket.on("ros2-topic-field", msg => quickLook.updateTopicField(socket, msg))
+    socket.on("ros2-message", msg => {
+        Graph("#chart", socket).push(msg.topic_name, msg.data, role)
+    })
+}
 
 $(document).ready(main)
 $(document).ready(Sub)
 $(document).ready(sub2)
+$(document).ready(sub3)
