f330f73a59d8e01cba006bb8dc98f175c9b333eb
KaoruNishikawa <kq.nishikawa@gmail.com>
2025-04-22 15:37:27 +0000

    Change font-family for entire page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import './globals.scss'
 import type { Metadata } from 'next'
 import { SocketProvider } from "@/providers/SocketProvider"
 import { SnackbarProvider } from "@/providers/SnackbarProvider"
+import { Quicksand } from 'next/font/google'
+
+const font = Quicksand({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
     title: 'Observer',
@@ -12,7 +15,7 @@ export const metadata: Metadata = {
 export default function Layout(props: { children: ReactNode }): ReactNode {
     return (
         <html lang='en'>
-            <body>
+            <body className={font.className}>
                 <header>
                     <a href="/">Home</a>
                     <a href="/qlook">Q-Look</a>
